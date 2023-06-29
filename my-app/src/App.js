5fe6ee5268bcd45843ee62eab4f02a73b9da8831
Vidssk <santiagohernandez120199@gmail.com>
2023-06-28 22:12:51 -0700

    Props tutorial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,25 @@
 
 import './App.css';
+import {useState} from 'react';
 import Employee from './components/Employee'
 
 function App() {
+  const [role, setRole] = useState('dev');
   console.log('we are about to list the employees');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? 
-      (<>
-        <Employee />
+      (
+      <>
+      <input type='text' onChange={(e) => {
+        console.log(e.target.value);
+        setRole(e.target.value);
+      }}
+      />
+        <Employee name="Santiago" role="Intern"/>
+        <Employee name="Chris" role={role}/>
+        <Employee name="Jose" role="Intern"/>
       </>
       ):(
         <p> you cannot see the employees</p>
