dc36ee2f6a42a3b1a8a884b9259c21fe01201f67
Vidssk <santiagohernandez120199@gmail.com>
2023-07-12 18:09:31 -0700

    Create active links using NavLink

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { NavLink } from 'react-router-dom'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Employees', href: '/Employees'},
+  { name: 'Customers', href: '/Customers'},
+  { name: 'Projects', href: '/other'},
+  { name: 'Calendar', href: '/'},
 ]
 
 function classNames(...classes) {
@@ -48,17 +49,21 @@ export default function Header(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'no-underline bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'no-underline rounded-md px-3 py-2 text-sm font-medium'
-                        )}
+                        to={item.href}
+                        // className={classNames(
+                        //   item.current ? 'no-underline bg-gray-900 text-white' : 
+                        // )}
+                        className={({isActive})=> {
+                          console.log(item.href + ' ' + isActive);
+                             return 'no-underline rounded-md px-3 py-2 text-sm font-medium' +
+                             (!isActive ? 'no-underline text-gray-300 hover:bg-gray-700 hover:text-white' : 'no-underline bg-gray-900 text-white' );
+                        }} 
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
