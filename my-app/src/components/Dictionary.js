55a115dd500999a9ec33859343e0772e6af7f77e
Vidssk <santiagohernandez120199@gmail.com>
2023-07-12 19:27:05 -0700

    useEffect different cases

--- my-app/src/components/Dictionary.js
+++ my-app/src/components/Dictionary.js
@@ -2,10 +2,24 @@ import { useState, useEffect } from "react";
 
 export default function Dictionary() {
     const [word, setWord] = useState('');
+    const [word2, setWord2] = useState('');
+
     // Limit what state useEffect cares about ---> dependency array
     // useEffect(()=>{console.log('state Updated ', word);})
-    useEffect(()=>{console.log('state Updated ', word);})
 
+    
+    //no dependency array ---> update for any sate change
+    // useEffect(()=>{console.log('state Updated ', word + ' ' + word2);})
+    
+    //empty dependency array ---> execute once 
+    // if you want to do something at initial page load (fetch data to put in web page)
+    // useEffect(()=>{console.log('state Updated ', word + ' ' + word2);},[]);
+    
+    // passing in data ---> only execute when those state variables are changed
+    // useEffect(()=>{console.log('state Updated ', word + ' ' + word2);},[word]);
+    useEffect(()=>{console.log('state Updated ', word);},[word]);
+    useEffect(()=>{console.log('state Updated ', word2);},[word2]);
+    
     return (
         <>
             <input type="text" onChange={ (e) => {
@@ -13,6 +27,11 @@ export default function Dictionary() {
             }
             }/>
             <h1>Let's get the definition for {word}</h1>
+            <input type="text" onChange={ (e) => {
+                setWord2(e.target.value);
+            }
+            }/>
+            <h2>Let's get the definition for {word2}</h2>
         </>
     )
 }
